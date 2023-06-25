b36615d3dc91df24c59c2be46ac65e773fdb7a6c
Rakib <rakibhasan75089@gmail.com>
2023-06-25 22:38:41 +0600

    localStorage problem solved.. Alhamdullah...

--- JS/cart.js
+++ JS/cart.js
@@ -13,8 +13,9 @@ const displayProduct = (product,quantity) =>{
     const li = document.createElement('li');
     li.innerText  = `${product}: ${quantity}`;
     ul.appendChild(li);
-    getStoredShoppingCart();
+    saveProductToLocalStorage(product,quantity);
 }
+
 
 const getStoredShoppingCart = () => {
     let cart = {};
@@ -26,5 +27,18 @@ const getStoredShoppingCart = () => {
 }
 
 const saveProductToLocalStorage = (product, quantity) => {
-
+    const cart = getStoredShoppingCart();
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
 }
+const displayProductsFromLocalStorage = () => {
+    const savedCart  = getStoredShoppingCart();
+    console.log(savedCart);
+    for(const product in savedCart){
+        const quantity = savedCart[product];
+        console.log(product, quantity);
+        displayProduct(product,quantity);
+    }
+}
+displayProductsFromLocalStorage();
